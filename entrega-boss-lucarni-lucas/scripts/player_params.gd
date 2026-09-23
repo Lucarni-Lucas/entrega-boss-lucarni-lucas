838afca2f3d7db03351d486d8e200a41d5937e9f
Lucas Nahuel Lucarni <lucaslucarni@yahoo.com.ar>
2026-09-23 03:35:38 -0300

    actualizo player_params.gd

--- entrega-boss-lucarni-lucas/scripts/player_params.gd
+++ entrega-boss-lucarni-lucas/scripts/player_params.gd
@@ -36,35 +36,35 @@ class_name PlayerParams
 @export_range(0, 1, 0.05) var sombra_alpha_min: float = 0.4
 
 @export_group("Cuerpo")
-@export_range(0, 200, 1, "suffix:px") var alto_cuerpo: float = 20.0
+@export_range(0, 200, 1, "suffix:px") var alto_cuerpo: float = 64.0
 
 @export_group("Slash")
 @export var slash_dano: int = 1
-@export_range(0, 1, 0.01, "suffix:s") var slash_duracion: float = 0.1
+@export_range(0, 1, 0.01, "suffix:s") var slash_duracion: float = 0.15
 @export_range(0, 2, 0.01, "suffix:s") var slash_cooldown: float = 0.3
-@export_range(0, 200, 5, "suffix:px") var slash_alcance: float = 50.0
+@export_range(0, 200, 5, "suffix:px") var slash_alcance: float = 90.0
 @export_range(0, 360, 5, "suffix:°") var slash_arco: float = 120.0
-@export_range(0, 200, 5, "suffix:px") var slash_radio_efecto: float = 50.0
+@export_range(0, 200, 5, "suffix:px") var slash_radio_efecto: float = 60.0
 @export_range(0, 0.5, 0.01, "suffix:s") var slash_estela_desvanecer: float = 0.12
 
 @export_group("Ground pound")
-@export_range(0, 300, 5, "suffix:px") var gp_altura_minima: float = 40.0
-@export_range(0, 0.5, 0.01, "suffix:s") var gp_anticipacion: float = 0.12
-@export_range(0, 5000, 50, "suffix:px/s") var gp_velocidad: float = 1500.0
+@export_range(0, 300, 5, "suffix:px") var gp_altura_minima: float = 80.0
+@export_range(0, 0.5, 0.01, "suffix:s") var gp_anticipacion: float = 0.22
+@export_range(0, 5000, 50, "suffix:px/s") var gp_velocidad: float = 2000.0
 @export var gp_dano: int = 1
 @export_range(0, 300, 5, "suffix:px") var gp_radio_impacto: float = 50.0
 @export var gp_radio_explosion: float = 0.0
-@export_range(0.03, 0.3, 0.01, "suffix:s") var gp_impacto_duracion: float = 0.05  
+@export_range(0.03, 0.3, 0.01, "suffix:s") var gp_impacto_duracion: float = 0.07  
 
 @export_group("Dive")
-@export_range(0, 3000, 5, "suffix:px/s") var dive_velocidad: float = 900.0
-@export_range(0, 2000, 5, "suffix:px/s") var dive_impulso_vertical: float = 500.0
+@export_range(0, 3000, 5, "suffix:px/s") var dive_velocidad: float = 950.0
+@export_range(0, 2000, 5, "suffix:px/s") var dive_impulso_vertical: float = 550.0
 @export_range(0, 1, 0.05) var dive_escala_gravedad: float = 1.0
 @export var dive_cargas_max: int = 1         
 
 @export_group("Boomerang")
 @export_range(0, 3000, 10, "suffix:px/s") var boomerang_velocidad: float = 1200.0
-@export_range(0, 1000, 10, "suffix:px") var boomerang_distancia: float = 350.0
+@export_range(0, 1000, 10, "suffix:px") var boomerang_distancia: float = 190.0
 @export_range(0, 5, 0.1, "suffix:s") var boomerang_duracion: float = 1.2
 @export_range(0, 3000, 10, "suffix:px/s") var boomerang_velocidad_vuelta: float = 1400.0
 @export_range(0, 1, 0.01, "suffix:s") var throw_duracion: float = 0.15
